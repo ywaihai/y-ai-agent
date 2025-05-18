2e38657b356e55b0a41b4827688b7bb028bd372f
ywaihai <y2054507614@163.com>
2025-05-18 17:07:17 +0800

    add 实现简单多轮对话并测试

---
+++ src/main/java/com/waihai/yaiagent/app/JavaMentorApp.java
@@ -0,0 +1,53 @@
+package com.waihai.yaiagent.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
+
+@Slf4j
+@Component
+public class JavaMentorApp {
+
+    private final ChatClient chatClient;
+
+    private static final String SYSTEM_PROMPT =
+            """
+            你是一位拥有 10 年 Java 全栈开发经验的资深导师。遵循以下原则：
+            1. 永远以引导代替直接答案，通过连续提问帮助用户理清问题本质
+            2. 根据用户表现动态调整教学策略（新手→原理拆解，进阶→最佳实践，专家→架构权衡）
+            3. 技术建议必须标注适用场景和版本限制（如Java 8/17/21特性差异）
+            4. 对不确定的结论主动声明"建议通过单元测试验证
+            """;
+
+    public JavaMentorApp(ChatModel dashscopeChatModel) {
+        ChatMemory chatMemory = new InMemoryChatMemory();
+        chatClient = ChatClient.builder(dashscopeChatModel)
+                .defaultSystem(SYSTEM_PROMPT)
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory)
+                )
+                .build();
+    }
+
+    public String doChat(String message, String chatId) {
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .chatResponse();
+
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
+}
---
+++ src/test/java/com/waihai/yaiagent/app/JavaMentorAppTest.java
@@ -0,0 +1,33 @@
+package com.waihai.yaiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JavaMentorAppTest {
+
+    @Resource
+    private JavaMentorApp javaMentorApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是刚入门的Java小白，叫waihai";
+        String answer = javaMentorApp.doChat(message, chatId);
+        // 第二轮
+        message = "我想学习Web开发方向";
+        answer = javaMentorApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我叫什么？刚跟你说过，帮我回忆一下";
+        answer = javaMentorApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
